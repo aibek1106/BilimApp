734c9097d05220ec090303fe5e8080d4278471ff
Aibek Shamshiev <ashamshiev@nurtelecom.kg>
2025-06-13 10:28:54 +0600

    init project

---
+++ src/main/java/kg/bilim_app/BilimAppApplication.java
@@ -0,0 +1,13 @@
+package kg.bilim_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilimAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilimAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/bilim_app/BilimAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bilim_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilimAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

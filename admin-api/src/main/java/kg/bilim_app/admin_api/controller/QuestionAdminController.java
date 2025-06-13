ea865b3e9e70e81482286d842651bbb0345b5b01
aibek1106 <92970752+aibek1106@users.noreply.github.com>
2025-06-13 11:16:31 +0600

    Add admin API module with question upload

---
+++ admin-api/src/main/java/kg/bilim_app/admin_api/controller/QuestionAdminController.java
@@ -0,0 +1,28 @@
+package kg.bilim_app.admin_api.controller;
+
+import kg.bilim_app.admin_api.dto.NewQuestionRequest;
+import kg.bilim_app.admin_api.service.QuestionAdminService;
+import kg.bilim_app.ort.entities.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin/questions")
+@RequiredArgsConstructor
+public class QuestionAdminController {
+
+    private final QuestionAdminService service;
+
+    @PostMapping
+    public Question create(@RequestBody NewQuestionRequest request) {
+        return service.createQuestion(request);
+    }
+
+    @PostMapping("/upload")
+    public void upload(@RequestParam("file") MultipartFile file) throws IOException {
+        service.uploadQuestions(file);
+    }
+}

5a897feaa10906c2d4d571ee9d1e814164091e1d
aibek1106 <92970752+aibek1106@users.noreply.github.com>
2025-06-14 10:29:11 +0600

    Add helper to fetch authenticated user

--- mobile-api/src/main/java/kg/bilim_app/mobile_api/controller/SubjectController.java
+++ mobile-api/src/main/java/kg/bilim_app/mobile_api/controller/SubjectController.java
@@ -1,13 +1,11 @@
 package kg.bilim_app.mobile_api.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kg.bilim_app.common.enums.Language;
 import kg.bilim_app.mobile_api.response.SubjectResponse;
 import kg.bilim_app.mobile_api.service.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +19,7 @@ public class SubjectController {
 
     @Operation(summary = "Get list of subjects with subgroups")
     @GetMapping
-    public List<SubjectResponse> getSubjects(@RequestParam Language language) {
-        return service.getSubjects(language);
+    public List<SubjectResponse> getSubjects() {
+        return service.getSubjects();
     }
 }
